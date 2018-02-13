893976d131e501f27100bcae154fce37dfa84970
malin4 <2rmalinowski@gmail.com>
2018-02-13 12:39:23 +0100

    my_blog_during_deploy

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,11 @@ app_name = 'blog'
 
 urlpatterns = [
     # Post views
-    path('', views.post_list, name='post_list'),
-    # path(views.PostListView.as_view(), name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path(views.PostListView.as_view(), name='post_list'),
     path('post/<int:year>/<int:month>/<int:day>/', views.post_detail, name='post_detail')
+
+
+
+
 ]
